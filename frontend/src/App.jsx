98e74639c0aaa5c5bee5d81a962f895524ab54f7
oleg venzhega <oleg.venzhega@gmail.com>
2025-01-19 20:30:14 -0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import { Note } from "./components/Note";
+import noteService from "./services/notes";
+import { Notification } from "./components/Notification";
+import { Footer } from "./components/Footer";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    console.log("effect");
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
+  }, []);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important == true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    };
+
+    noteService.create(noteObject).then((newNode) => {
+      setNotes(notes.concat(newNode));
+      setNewNote("");
+    });
+  };
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value);
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((n) => (n.id === id ? returnedNote : n)));
+      })
+      .catch((error) => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(() => notes.filter((n) => n.id != id));
+      });
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input type="text" value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+  const footerStyle = {
+    color: "green",
+    fontStyle: "italic",
+    fontSize: 16,
+  };
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2024
+      </em>
+    </div>
+  );
+};
