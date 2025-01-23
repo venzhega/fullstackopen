0899b19ed5b16ace00aae5c232ec23ec8a5a1a50
oleg venzhega <oleg.venzhega@gmail.com>
2025-01-22 19:00:54 -0500

    part-4-d

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,10 +1,19 @@
 const notesRouter = require("express").Router();
+const jwt = require("jsonwebtoken");
 const Note = require("../models/note");
+const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 notesRouter.get("/", async (request, response) => {
-  await Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
+  response.json(notes);
 });
 
 notesRouter.get("/:id", async (request, response) => {
@@ -19,12 +28,21 @@ notesRouter.get("/:id", async (request, response) => {
 notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id,
   });
 
   const savedNote = await note.save();
+  user.notes = user.notes.concat(savedNote._id);
+  await user.save();
   return response.status(201).json(savedNote);
 });
 
--- backend/tests/node_api.test.js
+++ backend/tests/node_api.test.js
@@ -2,11 +2,13 @@ const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const bcrypt = require("bcrypt");
 const app = require("../app");
 const api = supertest(app);
 
 const helper = require("./test_helper");
 
+const User = require("../models/user");
 const Note = require("../models/note");
 
 describe("when there are some notes saved initially", () => {
@@ -108,6 +110,57 @@ describe("when there are some notes saved initially", () => {
 
       const contents = notesAtEnd.map((r) => r.content);
       assert(!contents.includes(noteToDelete.content));
+    });
+  });
+
+  describe("when there is initially one user in db", () => {
+    beforeEach(async () => {
+      await User.deleteMany({});
+      const passwordHash = await bcrypt.hash("sekret", 10);
+      const user = new User({ username: "root", passwordHash });
+      await user.save();
+    });
+
+    test("creation succeeds with a fresh username", async () => {
+      const usersAtStart = await helper.usersInDb();
+
+      const newUser = {
+        username: "mluukkai",
+        name: "Matti Luukkainen",
+        password: "salainen",
+      };
+
+      await api
+        .post("/api/users")
+        .send(newUser)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+
+      const usersAtEnd = await helper.usersInDb();
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1);
+
+      const usernames = usersAtEnd.map((u) => u.username);
+      assert(usernames.includes(newUser.username));
+    });
+
+    test("creation fails with proper statuscode and message if username already taken", async () => {
+      const usersAtStart = await helper.usersInDb();
+
+      const newUser = {
+        username: "root",
+        name: "Superuser",
+        password: "salainen",
+      };
+
+      const result = await api
+        .post("/api/users")
+        .send(newUser)
+        .expect(400)
+        .expect("Content-Type", /application\/json/);
+
+      const usersAtEnd = await helper.usersInDb();
+      assert(result.body.error.includes("expected `username` to be unique"));
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length);
     });
   });
 });
